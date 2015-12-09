b926822692fb12f00b6200a449a9bf596784be05
João Torquato <joao.otl@gmail.com>
2015-12-09 21:42:42 -0200

    finalizado

---
+++ app/controllers/companies_controller.rb
@@ -0,0 +1,21 @@
+class CompaniesController < ApplicationController
+
+  def new
+    @company = Company.new
+  end
+
+  def create
+    @company = Company.create(company_params)
+    render 'show'
+  end
+
+  def show
+    @company = Company.find(params[:id])
+  end
+
+  private
+
+  def company_params
+    params.require(:company).permit(:name, :location, :mail, :phone)
+  end
+end
